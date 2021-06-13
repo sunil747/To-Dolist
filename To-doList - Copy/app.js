fe657d46b458987bb5195cf180af3bc96fe1a457
sunil747 <30520358+sunil747@users.noreply.github.com>
2021-06-13 15:11:26 +0530

    Add files via upload
    Simple todo list app made in NodeJS.

---
+++ To-doList - Copy/app.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const app = express();
+
+var items =["Food", "water"];
+var workItems = ["pen", "book"];
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
+
+
+
+app.get("/", function(req, res){
+  //res.sendFile(__dirname+'/index.html')
+
+  const today = new Date();
+  var options = {
+    weekday:"long",
+    day:"numeric",
+    month:"long"
+  };
+
+  var day = today.toLocaleDateString("en-US", options);
+  res.render("list",{
+    listTitle:day,
+    newListItem:items
+  });
+
+});
+
+app.post("/", function(req, res){
+  item = req.body.newItem;
+  if(req.body.list="Work"){
+      workItems.push(item);
+      res.redirect("/work");
+  }
+  else {
+    items.push(item);
+    res.redirect("/");
+  }
+
+});
+
+app.get("/work", function(req, res){
+  res.render("list",{
+    listTitle:"Work",
+    newListItem:workItems
+
+  });
+});
+
+
+
+app.listen(3000, function(){
+  console.log("server started on port 3000");
+});
